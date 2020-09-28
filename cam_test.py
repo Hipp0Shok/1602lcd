1068e5b3aa7fa1ed76817406c065e1459a9dc993
Hipp0Shok <gleb.zhivotov@mail.ru>
2020-09-28 05:34:58 -0700

    Cam now working with button and lcd

--- cam_test.py
+++ cam_test.py
@@ -1,5 +1,26 @@
+BUTTON_PIN = 14
+
 import cv2
+import lcddriver
+
+import sys
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+lcd = lcddriver.lcd()
 cam = cv2.VideoCapture(0)
+
+print("push 3 times")
+click_cnt = 0
+while click_cnt < 3:
+	inputValue = GPIO.input(BUTTON_PIN)
+	if(inputValue == True):
+		click_cnt = click_cnt + 1
+		print(click_cnt)
+	sleep(10)
+lcd.lcd_display_string("Show your sign", 1)
 while(True):
     ret, img = cam.read()
     cv2.imshow('my_cam', img)
@@ -7,3 +28,8 @@ while(True):
 	    break
 cam.release()
 cv2.destroyAllWindows()
+
+cam.release()
+cv2.destroyAllWindows()
+print("done")
+GPIO.cleanup()
